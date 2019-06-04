e6a568c97aeb6290b052af5099dd04faf716fd2b
kimura kouhei <kimura kouhei@DESKTOP-KB46VQ2>
2019-06-04 17:33:56 +0900

    メニュー編集

--- src/servlet/Menu.java
+++ src/servlet/Menu.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;		// 追加したインポート文
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Menu")
 public class Menu extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -25,17 +27,30 @@ public class Menu extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// リクエスト先の指定
+		RequestDispatcher dispatcher = request.getRequestDispatcher(
+				"/META-INF/jsp/menu.jsp");
+		dispatcher.forward(request, response);
+
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response)
+			throws ServletException, IOException {
+		// リクエスト先の指定
+		RequestDispatcher dispatcher = request.getRequestDispatcher(
+				"/META-INF/jsp/menu.jsp");
+		dispatcher.forward(request, response);
+
+		//doGet(request, response);
 	}
 
 }

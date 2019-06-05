3ae7a9007870150bc7a98cce211d2431631353e9
uchida-riku <uchida-riku@DESKTOP-EC4LVVB>
2019-06-05 10:47:43 +0900

    編集

--- src/servlet/Login.java
+++ src/servlet/Login.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -38,4 +39,4 @@ public class Login extends HttpServlet {
 		doGet(request, response);
 	}
 
-}
+}// 完了

cc667953512471b637f38a6a2f56753caf499b8c
kimura kouhei <kimura kouhei@DESKTOP-KB46VQ2>
2019-06-05 10:29:00 +0900

    Division_DAOの変更
    データベース接続Sample作成

--- src/dao/Division_DAO.java
+++ src/dao/Division_DAO.java
@@ -2,26 +2,27 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-
-//import servlet.Division 	 // 表示する先のインポート
-
 
 
 public class Division_DAO {
 	// データベース接続に使用する情報
-	private final String JDBC_URL =
-			"";								// 接続の情報
-	private final String DB_USER = ""; 	// 接続先のユーザー名
-	private final String DB_PASS = "";		// 接続先のパスワード
+	private static String DRIVER_NAME = "";	// ドライバの名前
+	private static String JDBC_URL ="";		// 接続先のURL
+	private static String DB_USER = ""; 		// 接続先のユーザー名
+	private static String DB_PASS = "";		// 接続先のパスワード
 
-	//public Division findByLogin(Login login) {
-		//Division division = null;
-
-	// データベースへ接続
-	//try()
-
-	//}
+	// データベースに接続
+	public static Connection getConnection() {
+		Connection con = null;
+		try {
+			Class.forName(DRIVER_NAME);
+			con = DriverManager.getConnection(JDBC_URL,DB_USER,DB_PASS);
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 }

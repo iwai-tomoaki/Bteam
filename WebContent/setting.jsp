b824833a41a380a284e8f95e337a2e8d0cc6d1e6
iwai tomoaki <iwai tomoaki@DESKTOP-UFMS125>
2019-06-06 13:08:26 +0900

    更新

--- WebContent/setting.jsp
+++ WebContent/setting.jsp
@@ -5,25 +5,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="css/Style.css">
+<link rel="stylesheet" type="text/css" href="css/Stayle.css">
 <meta charset="UTF-8">
 <title>パスワード再設定</title>
 </head>
-<% if(loginUser.equals(true)) {%>	<%--未確認エラー(equalsで代用してエラーを消している) --%>
-<body class="sett">
+<% %>	<%--未確認エラー(equalsで代用してエラーを消している) --%>
+<body class="set">
+
+h
 <form action="/Bteam/Setting" method="post">
 社員番号:<input type="text" name="pass"><br>
 再設定のパスワード:<input type="password" name="newpass"><br>
 再設定のパスワード:<input type="password" name="newpass"><br>
 <input type="submit" value="再設定">
 </form>
-<% } else { %>
+<%  %>
 <form action="/Bteam/Setting" method="post">
 前のパスワード:<input type="text" name="pass"><br>
 新しいパスワード:<input type="password" name="newpass"><br>
 新しいパスワード:<input type="password" name="newpass"><br>
 <input type="submit" value="再設定">
 </form>
-<% } %>
+<%  %>
 </body>
 </html>
--- src/servlet/Setting.java
+++ src/servlet/Setting.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,16 +28,18 @@ public class Setting extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(
+				"/META-INF/jsp/setting.jsp");
+		dispatcher.forward(request, response);
+
 	}
 
 }

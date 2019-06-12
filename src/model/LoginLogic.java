4363df44b9777cab70388c189dae7c054800cfa4
hisamoto <hisamoto@DESKTOP-BNO2RUI>
2019-06-12 09:35:56 +0900

    コメント追加

--- src/model/LoginLogic.java
+++ src/model/LoginLogic.java
@@ -12,10 +12,10 @@ public class LoginLogic extends EmployeeDAO{
 		for(int roop_order = 0; roop_order < roop; roop_order ++) {		//取得した要素数分ループ
 			int emp_num_order = emp_num.get(roop_order);			//ArrayList<Integer>の(roop_order)番目の中身をint型に変換してuser_num_orderに代入
 			String pass_order = pass.get(roop_order);	//ArrayList<String>の(roop_order)番目の中身をString型に変換してuser_pass_orderに代入
-			String user_name_order = emp_name.get(roop_order);
 			if(emp_num_order == input_num && pass_order.equals(input_pass)){		//入力した社員番号とパスワードがテーブル内から取得した社員番号とパスワードに一致するか判定
 				this.true_or_false = true;		//一致した場合true_or_falseにtrueを入れる
-				this.user_name = user_name_order.toString();
+				String user_name_order = emp_name.get(roop_order);		//ArrayList<String>の(roop_order)番目の中身をString型に変換してuser_name_orderに代入
+				this.user_name = user_name_order;		//取得したuser_name_orderをuser_nameに代入
 				return;		//社員番号とパスワードが一致するのは一組だけなので一致したものがあったらreturnでLoginサーブレットに戻る
 			}
 		}

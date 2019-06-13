16401b0cfcb5a3d92ef72aafaa5d9ac7014bc0a1
ichimoto <ichimoto@DESKTOP-63IR9PK>
2019-06-13 13:22:09 +0900

    ManagemmentMenu.javaを作成

---
+++ src/servlet/ManagementMenu.java
@@ -0,0 +1,39 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;		// 追加したインポート文
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.User;
+
+/**
+ * Servlet implementation class Menu
+ */
+@WebServlet("/ManagementMenu")
+public class ManagementMenu extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//ログインしているか確認するため
+		//セッションスコープからユーザー情報を取得
+		HttpSession session = request.getSession();
+		User loginUser = (User) session.getAttribute("loginUser");
+
+		//ログインしていない場合
+		if (loginUser == null) {
+			//リダイレクト
+			response.sendRedirect("/Bteam/");
+		} else { //ログイン済みの場合
+			//フォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/management_menu.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+}

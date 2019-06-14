ccf9a658102f085627b32aacfd5303f38222ede9
okada kengo <okada kengo@DESKTOP-CVT99MO>
2019-06-14 15:47:03 +0900

    ログアウト処理の実施

---
+++ src/servlet/Logout.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+
+		// セッションの破棄
+		HttpSession session = request.getSession();
+		session.invalidate();
+
+		//フォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}

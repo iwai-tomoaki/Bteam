ef52ffa738d9ec2d8b0fc4428786dc5bbfd8e33a
hisamoto <hisamoto@DESKTOP-BNO2RUI>
2019-06-20 13:52:11 +0900

    コミット

--- src/servlet/Login.java
+++ src/servlet/Login.java
@@ -14,8 +14,7 @@ import model.LoginLogic;
 import model.User;
 
 @WebServlet("/Login")
-public class Login extends HttpServlet
-{
+public class Login extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request,HttpServletResponse response)throws ServletException, IOException{
